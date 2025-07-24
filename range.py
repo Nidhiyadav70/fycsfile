69590db076c3906345e5f04161fa876176e8312a
Nidhiyadav70 <sulabiyadav774@gmail.com>
2025-07-24 16:52:25 +0530

    Create range.py

---
+++ range.py
@@ -0,0 +1,35 @@
+for i in range(10):
+    if i == 5:
+        break
+    print(i)
+
+0 1 2 3 4
+
+for i in range(10):
+    if i == 5:
+        continuewhile True:
+    name = input("Enter your name (type exit to stop):")
+    if name == "exit":
+        break
+    print('Hello', name)
+    print(i)
+0
+1
+2
+3
+4
+6
+7
+8
+9
+
+for i in range(5):
+    if i == 2:
+        pass
+    print(i)
+
+0
+1
+2
+3
+4
